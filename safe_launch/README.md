3033cf71a4bc98465cbed9b1a777d76c4c8da44f
odisfm <102568741+odisfm@users.noreply.github.com>
2024-09-19 13:56:13 +1000

    Initial commit

---
+++ safe_launch/README.md
@@ -0,0 +1,24 @@
+# safelaunch
+
+Launches a scene, only if it isn't empty. Shouts at you<sup>1</sup> if you try to launch an empty scene.
+
+## usage
+`SAFELAUNCH <scene number>`\
+`SAFELAUNCH SEL` launch the selected scene\
+`SAFELAUNCH NEXT` launch the scene below the selected scene
+
+### checking within a range of tracks
+
+Optionally, you can specify a range of tracks, and only those tracks will be considered when determining if a scene is empty: at least one clip must be present on a track within this range for the scene to launch.
+
+You can specify this range two ways:
+
+#### globally in safe_launch.py
+
+Near the top are the variables `FIRST_TRACK` and `LAST TRACK`, set to `None` by default. Change these to the names of your tracks, or leave them as `None`.
+
+#### as arguments to the action
+
+You can set the first or the first __and__ last tracks to consider when calling the action like `SCENELAUNCH SEL "drums" "keys"`. Double-quotes are mandatory (specifying tracks by number is not supported). Only specifying the first track will leave the last track as the value defined in __safe_launch.py__.
+
+<sup>1</sup> Shows an error in the Live UI, in the log, and on a connected Push 1 or Push 2. Push 2 in Live 12+ only supported if used with the legacy control surface script.
---
+++ safe_launch/safe_launch.py
@@ -0,0 +1,113 @@
+# made by odis :)
+# https://github.com/odisfm/clyph-x-actions
+
+from ClyphX_Pro.clyphx_pro.UserActionsBase import UserActionsBase
+
+FIRST_TRACK = None
+LAST_TRACK = None
+
+class safe_launch(UserActionsBase):
+
+    ### boilerplate
+
+    def __init__(self, *a, **k):
+        super().__init__(*a, **k)
+        self.logging_level = 'critical'
+
+    def log(self, message, critical = False):
+        if self.logging_level != 'all' and critical == False:
+            return
+        if critical:
+            message = 'CRITICAL: ' + message
+        self.canonical_parent.log_message(f'{self.__class__.__name__.upper()}: {message}')
+
+    def msg(self, message):
+        self.canonical_parent.show_message(f'{self.__class__.__name__.upper()}: {message}')
+
+    def cxp_action(self, action_list):
+        self.canonical_parent.clyphx_pro_component.trigger_action_list(action_list)
+
+        ### extras
+
+    def get_selected_scene_index(self):
+        return list(self._song.scenes).index(self._song.view.selected_scene)
+
+    def get_track_by_name(self, search_name):
+        for track in list(self._song.tracks):
+            if track.name == search_name:
+                return track
+        self.log('no track matching search term!', critical=True)
+        return False
+
+    def pushmsg(self, message, class_label = False):
+        if not class_label:
+            self.cxp_action(f'PUSH MSG "{message}"')
+        else:
+            self.cxp_action(f'PUSH MSG "{self.__class__.__name__.upper()}: {message}"')
+
+    def shout(self, message, critical = False, class_label = False):
+        self.log(message, critical)
+        self.msg(message)
+        self.pushmsg(message, class_label)
+
+    ### end boilerplate
+
+    def create_actions(self):
+        self.add_global_action('safelaunch', self.safe_launch)
+
+    def safe_launch(self, action_def, args):
+        self.log('running')
+        arg_scene = args.split(' ')[0]
+        if arg_scene == '':
+            self.log('must specify scene number, \'NEXT\', or \'SEL\'!', critical=True)
+            return
+        if arg_scene == 'next':
+            scene_index = self.get_selected_scene_index() + 1
+        elif arg_scene == 'sel':
+            scene_index = self.get_selected_scene_index()
+        else:
+            try:
+                args = int(args)
+                scene_index = args - 1
+            except:
+                self.log('must specify scene number as integer!', critical=True)
+                return
+
+        first_track = FIRST_TRACK
+        last_track = LAST_TRACK
+
+        arg_quotes = args.split('"')
+
+        if len(arg_quotes) >= 2:
+            first_track = arg_quotes[1]
+            if len(arg_quotes) >= 4:
+                last_track = arg_quotes[3]
+
+        scene_has_clip = False
+
+        if first_track == None:
+            started = True
+        else:
+            started = False
+
+        self.log(f'checking tracks between {first_track} and {last_track}')
+
+        for track in self._song.tracks:
+            self.log(f'checking track {track.name}')
+            if track.name == first_track:
+                started = True
+            if started == False:
+                continue
+            if track.name == last_track:
+                break
+            if track.clip_slots[scene_index].has_clip:
+                scene_has_clip = True
+                break
+
+        if not scene_has_clip:
+            message = f'Can\'t launch empty scene! ({scene_index + 1})'
+            self.shout(message, critical=True)
+            return
+        else:
+            self._song.scenes[scene_index].fire()
+            self._song.view.selected_scene = self._song.scenes[scene_index]
